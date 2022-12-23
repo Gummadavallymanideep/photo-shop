ddc86daaea3a80a7238e09cfaa4407766d9b24f3
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2022-12-23 21:12:50 +0530

    photoshop

---
+++ example-ae.js
@@ -0,0 +1,19 @@
+/**
+  JSONify the first layer of the fist composition of the current project
+*/
+
+require('./aftereffects').createStream(function jsx(stream, props){
+  
+    var composition = app.project.ao_comps()[0];
+    var layer = composition.layers[1];
+    
+    stream.writeln(
+      JSON.stringify(layer, null, 2)
+    );
+    
+    stream.writeln(JSON.stringify(props));
+    stream.close(); // If you don't do this then After Effects will never disconnect from node.js and everything will just sit there forever
+    
+  }, [{lulz:Math.random(0)}])
+  .pipe(process.stdout);
+  
